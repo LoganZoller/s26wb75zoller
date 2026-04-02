fa0631be6d9c077174f51b79aa2130b899eca2dd
Logan Zoller <s559751@nwmissouri.edu>
2026-04-01 22:36:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var gridRouter = require('./routes/grid');
 var indexRouter = require('./routes/index');
 var eggRouter = require('./routes/egg');
 var usersRouter = require('./routes/users');
-
+var gridRouter = require('./routes/grid');
 
 var app = express();
 
@@ -22,10 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/grid', gridRouter);
 app.use('/', indexRouter);
 app.use('/egg', eggRouter);
 app.use('/users', usersRouter);
+app.use('/grid', gridRouter);
 
 
 
