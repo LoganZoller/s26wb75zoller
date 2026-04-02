a9767e1be9d26668a024f227cf72b3b96f50503e
Logan Zoller <s559751@nwmissouri.edu>
2026-04-01 23:39:33 -0500

    created new pug and js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eggRouter = require('./routes/egg');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/egg', eggRouter);
 app.use('/grid', gridRouter);
+app.use('/pick', pickRouter);
 
 
 
